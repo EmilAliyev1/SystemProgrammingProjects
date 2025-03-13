1a8d2c67df4673eee09e0ee7cc29d35cab9d9e0f
Emil Aliyev <aliyev.emil2209@gmail.com>
2025-03-13 21:39:12 +0400

    finished task 1 on process lesson

--- ProcessLesson/Program.cs
+++ ProcessLesson/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+#region Task 1
+
+Console.WriteLine($"Process with id: {Process.GetCurrentProcess().Id} started");
+
+string childApp = "C:\\Users\\Aliev_uj65\\RiderProjects\\SystemProgrammingProjects\\ChildProcess\\bin\\Debug\\net8.0\\ChildProcess.exe";
+
+ProcessStartInfo psi = new ProcessStartInfo
+{
+    FileName = childApp,
+    Arguments = "arg1 arg2 arg3",
+    RedirectStandardOutput = true
+};
+
+using Process process = new Process { StartInfo = psi };
+
+process.EnableRaisingEvents = true;
+process.Exited += (sender, e) => Console.WriteLine($"Child process with id: {process.Id} finished");
+process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+
+process.Start();
+
+Console.WriteLine($"Child process with id: {process.Id} started");
+
+process.BeginOutputReadLine();
+
+process.WaitForExit();
+
+
+Console.WriteLine($"Process with id: {Process.GetCurrentProcess().Id} finished");
+
+#endregion
+
+#region Task 2
+
+
+
+#endregion
